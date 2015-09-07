6929e7c0ab8174cbd4d8bc7e1ff66877cd59178b
lorand <services@platinaxe.com>
2015-09-08 01:12:33 +0300

    added default template files

---
+++ public/default/gui/page.tpl.php
@@ -0,0 +1,5 @@
+<?php
+debug('Template loaded');
+debug('Here is a cute image to be happy');
+?>
+<img src="http://static.fjcdn.com/pictures/Finger+kitten+so+tiny+it+looks+like+it+s+on+someone+s_ac6710_4498729.jpg" />
