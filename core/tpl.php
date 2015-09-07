d49a201858daf25868fb0a21a35ff613ffa6ed84
lorand <services@platinaxe.com>
2015-09-08 01:11:56 +0300

    added template include

---
+++ core/tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+//	Print the page
+function tpl_render_page() {
+	$themename = tpl_get_theme_folder();
+	$themepage = tpl_get_page();
+	$extension = '.tpl.php';
+	include_once( '/public/' . $themename . '/gui/header' . $extension );
+	include_once( '/public/' . $themename . '/gui/' . $themepage . $extension );
+	include_once( '/public/' . $themename . '/gui/footer' . $extension );
+}
+
+function tpl_get_page() {
+	return 'page';
+}
+
+function tpl_get_theme_folder() {
+	$themename = query_get_setting( 'current_theme' );
+	if (file_exists( XENO_ROOT . '/public/' . $themename . '/info.txt' )) {
+		return $themename;
+	}
+	return 'default';
+}
+
+function tpl_get_css() {
+	return;
+}
+
+function tpl_get_js($location) {
+	return;
+}
